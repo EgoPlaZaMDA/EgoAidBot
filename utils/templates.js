4db0593c3ddf500c26860891c33c4471608caedd
EgoPlaZaMDA <egoplazamda@gmail.com>
2024-11-04 03:43:44 -0500

    Change inactivity timeout from 2 minutes to 24 hours

--- utils/templates.js
+++ utils/templates.js
@@ -8,15 +8,18 @@ export const followUp = async (client) => {
   for (const [id, thread] of channels) {
       const messages = await thread.messages.fetch({ limit: 1 });
       const lastMessage = messages.first();
+
+      if (!lastMessage) continue;
+
       const now = Date.now();
-      const inactivityLimit = 1000 * 60 * 2; // 2 minutos
+      const inactivityLimit = 1000 * 60 * 60 * 24; // 24 hours
 
       if (now - lastMessage.createdTimestamp > inactivityLimit) {
           const userId = lastMessage.author.id;
           await thread.send(`Hello <@${userId}>, this ticket has been closed due to inactivity. Please open a new ticket if the issue persists.`);
           
-          await thread.setLocked(true); 
-          await thread.setArchived(true); 
+          await thread.setLocked(true);
+          await thread.setArchived(true);
       }
   }
 }
