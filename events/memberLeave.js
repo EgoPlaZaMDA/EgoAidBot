b9f95a5334174259d61be1b07359bdf5cb179197
EgoPlaZaMDA <egoplazamda@gmail.com>
2024-11-04 16:14:30 -0500

    Add functionality to close tickets when a user leaves the server

--- events/memberLeave.js
+++ events/memberLeave.js
@@ -1,22 +1,24 @@
+import { closeTicket } from '../commands/closeTicket.js';
+
 export const memberLeave = async (member) => {
     const channels = member.guild.channels.cache.filter(channel => 
         channel.isThread() && 
         !channel.archived && 
-        (channel.name.startsWith('ticket-appeal-') || channel.name.startsWith('ticket-report-'))
+        (channel.name.startsWith('appeal-') || channel.name.startsWith('report-'))
     );
 
     channels.forEach(async (thread) => {
-        const owner = thread.name.split('-')[2]; 
+        const threadOwner = thread.name.split('-')[1];
 
-        if (owner === member.user.username) {
+        if (threadOwner === member.user.username) {
             await thread.send({
                 embeds: [{
-                    color: 0xff0000,  // Red color to indicate the ticket is closed due to user leaving
-                    description: `**Ticket Closed: User <@${member.user.id}> has left the server**`
+                    color: 0xff0000,
+                    description: `**Ticket Closed: User <@${member.user.id}> has left the server**. Please create a new ticket if you rejoin the server.`
                 }]
             });
             await thread.setLocked(true);
             await thread.setArchived(true);
         }
     });
-}
+};
